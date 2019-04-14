d2ff5b936f60a5e1a99946cdcc8dc11b27349c8b
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2019-04-13 20:55:12 -0500

    created navigation component

---
+++ src/app/classes/navigation-item.ts
@@ -0,0 +1,8 @@
+export class NavigationItem {
+  public title: String;
+  public route: String;
+  public url: String;
+  public type: String;
+  public icon: String;
+  public isMobile: Boolean;
+}
