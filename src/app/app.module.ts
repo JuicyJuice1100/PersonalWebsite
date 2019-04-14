dea43903fbbcaa8015a573c39faeca877a98e5f6
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2019-04-13 21:14:24 -0500

    Reorganized some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { Routing } from './app.routing';
 
 import { AppComponent } from './components/app/app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { AboutComponent } from './components/about/about.component';
+
+
 import { DocumentRefService } from './services/documentRef.service';
 
 @NgModule({
    declarations: [
       AppComponent,
       AboutComponent,
+      NavigationComponent
    ],
    imports: [
       BrowserModule,
