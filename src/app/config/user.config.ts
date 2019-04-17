1c3917dbba3e80672745025f28bf4cd9876867b6
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2019-04-17 17:54:27 -0500

    cleaned up stylesheets and added more color styles

--- src/app/config/user.config.ts
+++ src/app/config/user.config.ts
@@ -1,12 +1,13 @@
+// tslint:disable: max-line-length
 export const UserConfig = {
   home: {
     welcomeMsg: 'What\'s Up I\'m',
     name: 'Justin L. Espiritu',
     // title: 'Developer | Gamer | Rockclimber',
     titles: [
-      { title: 'Developer', class: 'title--hover-green' },
-      { title: 'Gamer', class: 'title--hover-red' },
-      { title: 'Rock Climber', class: 'title--hover-yellow' }
+      { title: 'Developer', class: 'element--hover-blue' },
+      { title: 'Gamer', class: 'element--hover-red' },
+      { title: 'Rock Climber', class: 'element--hover-yellow' }
     ]
   },
   about: {
@@ -17,8 +18,8 @@ export const UserConfig = {
     job: 'Teamcenter Implementation Engineer @ Mercury Marine'
   },
   social: [
-    { title: 'Github', url: 'https://github.com/juicyjuice1100/', icon: 'fab fa-github fa-2x'},
-    { title: 'LinkedIn', url: 'https://www.linkedin.com/in/justin-espiritu-b4a277138/', icon: 'fab fa-linkedin-in fa-2x'},
-    { title: 'Steam', url: 'https://steamcommunity.com/id/juicyjuice1100/', icon: 'fab fa-steam-symbol fa-2x'}
+    { title: 'Github', url: 'https://github.com/juicyjuice1100/', icon: 'fab fa-github fa-2x', class: 'element--hover-green'},
+    { title: 'LinkedIn', url: 'https://www.linkedin.com/in/justin-espiritu-b4a277138/', icon: 'fab fa-linkedin-in fa-2x', class: 'element--hover-blue'},
+    { title: 'Steam', url: 'https://steamcommunity.com/id/juicyjuice1100/', icon: 'fab fa-steam-symbol fa-2x', class: 'element--hover-yellow'}
   ]
 };
